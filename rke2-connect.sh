b4f6231f76ccf8e5a046f2f6914aca326c1f4a0f
blurdel <5648737+blurdel@users.noreply.github.com>
2025-07-12 19:40:22 -0400

    updates to check KUBECONFIG

--- rke2-connect.sh
+++ rke2-connect.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-if [[ $# -ne 1 ]]; then
-    echo "usage: $0 </path/kubeconfig.yaml>"
+# Save server yaml to ~/Downloads/local.yaml
+# Set: KUBECONFIG=~/Downloads/local.yaml
+# Run this script
+
+echo "KUBECONFIG=${KUBECONFIG}"
+if [[ -z "${KUBECONFIG}" ]]; then
+    echo "KUBECONFIG is not set, exiting..."
     exit
 fi
+echo "KUBECONFIG=${KUBECONFIG}"
 
-export KUBECONFIG="${1}"
-echo "${KUBECONFIG}"
 
-# Print config
+# Print some config items
 kubectl version --short
+# kubectl config get-clusters
 # kubectl config view
 
-# Set cluster
-kubectl config set-cluster local --insecure-skip-tls-verify=true
+# Our rke2 #.dev clusters are named local
+# kubectl config set-cluster local --insecure-skip-tls-verify=true
 
 kubectl get nodes
